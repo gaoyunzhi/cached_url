859a324ebdea75a7191e421b8cf34699a3a68234
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-30 20:01:35 -0800

    code complete

--- cached_url/__init__.py
+++ cached_url/__init__.py
@@ -2,114 +2,30 @@
 # -*- coding: utf-8 -*-
 
 name = 'cached_url'
-token = ''
+import os
 
-from html_telegraph_poster import TelegraphPoster
-from .article import _getArticle
-from .common import _seemsValidText
-from telegram_util import matchKey
+def getUrlContent(url):
+	headers = {
+		'method': 'GET',
+		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+		'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+	}
+	return requests.get(url, headers=headers).text
 
-def _getPoster():
-	global token
-	if token:
-		return TelegraphPoster(access_token = token)
-	p = TelegraphPoster()
-	r = p.create_api_token('cached_url', 'cached_url')
-	token = r['access_token']
-	return p
-
-def _trimUrl(url):
-	if not '://' in url:
-		return url
-	loc = url.find('://')
-	return url[loc + 3:]
-
-def _formaturl(url):
-	if '://' not in url:
-		return "https://" + url
-	return url
-
-def getArticle(url, throw_exception=False):
+def cachedContent(url):
+	cache = 'tmp/' + hashlib.sha224(url.encode('utf-8')).hexdigest()[:10] + '.html'
 	try:
-		return _getArticle(_formaturl(url))
-	except Exception as e:
-		if throw_exception:
-			raise e
-
-def isConfidentUrl(url):
-	return matchKey(url, ['mp.weixin.qq.com', 'stackoverflow', 'bbc', 'nyt', 'telegra'])
-
-def isConfident(url, soup):
-	if not isConfidentUrl(url):
-		return False
-	if not _seemsValidText(soup):
-		return False
-	for item in soup.find_all('figure'):
-		img = item.find('img')
-		if img and img.get('src', '').startswith('http'):
-			return True
-	return False
-
-def _getTelegraphPath(url):
-	marker = 'telegra.ph/'
-	index = url.find(marker)	
-	if index == -1:
-		return
-	return url[index + len(marker):]
+		with open(cache) as f:
+			return f.read()
+	except:
+		content = getUrlContent(url)
+		os.system('mkdir tmp > /dev/null 2>&1')
+		with open(cache, 'w') as f:
+			f.write(content)
+		return content
 
 def get(url):
-	p = _getPoster()
-	r = p._api_request('getPage', {
-		'path': _getTelegraphPath(url),
-		'fields': ['canEdit', 'can_edit'],
-		'access_token': token
-	})
-	return r.get('result', {})
-
-def _isEditable(p, url):
-	path = _getTelegraphPath(url)
-	if not path:
-		return False
-	r = p._api_request('getPage', {
-		'path': path,
-		'fields': ['can_edit'],
-		'access_token': token
-	})
-	if not r:
-		return False
-	r = r.get('result', {})
-	if r.get('can_edit'):
-		return True
-	# seems telegra.ph api stop to return the can_edit field, use confidenturl heuristics instead
-	return isConfidentUrl(r.get('author_url')) 
-
-def export(url, throw_exception=False, force=False):
-	try:
-		if not force and not isConfidentUrl(url):
-			return
-		p = _getPoster()
-		if not force and _isEditable(p, url):
-			return url
-		article = getArticle(url, throw_exception)
-		if not article.text or not article.text.text.strip():
-			article.text = '<div>TO BE ADDED</div>'
-		try:
-			r = p.post(
-				title = article.title, 
-				author = article.author, 
-				author_url = _formaturl(article.url or url), 
-				text = str(article.text))
-		except Exception as e:
-			if 'CONTENT_TEXT_REQUIRED' in str(e):
-				r = p.post(
-					title = article.title, 
-					author = article.author, 
-					author_url = _formaturl(article.url or url), 
-					text = '<div>TO BE ADDED</div>')
-			else:
-				raise e
-		if force or isConfident(url, article.text):
-			return _trimUrl(r['url'])
-	except Exception as e:
-		if throw_exception:
-			raise e
+	if 'test' in str(sys.argv):
+		return cachedContent(url)
+	else:
+		return getUrlContent(url)
