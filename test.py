bfdd00e7e1b52eebb38dee00368cf39bc69bcb01
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-30 19:54:19 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cached_url
+from cached_url import export, _formaturl
+import os
+import sys
+
+urls = [
+	'https://madeinchinajournal.com/2019/04/18/the-plight-of-sex-workers-in-china%ef%bb%bf-from-criminalisation-and-abuse-to-activism/'
+]
+
+def _test():
+	if len(sys.argv) > 1:
+		mode = sys.argv[1]
+	else:
+		mode = ''
+	if mode == 'open':
+		mode = ''
+	for url in urls:
+		if not mode in url:
+			continue
+		print('原文：', url)
+		r = export(url, True, True)
+		print('导出：', r)
+		if 'open' in str(sys.argv):
+			os.system('open ' + _formaturl(r) + ' -g')
+		print('')
+
+_test()
