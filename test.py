38a31b94f42304b5afb6a1964bc1ec39d85779ec
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-15 09:11:22 -0700

    commit

--- test.py
+++ test.py
@@ -4,3 +4,4 @@
 import cached_url
 
 print(cached_url.get('https://www.bbc.com/zhongwen/simp'))
+print(cached_url.get('https://img9.doubanio.com/view/status/l/public/64f0c054cbc6f3d.jpg', mode='b'))
