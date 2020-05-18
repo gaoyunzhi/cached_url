003520b0badf8977c8976353e2d6ccac2a8c9bde
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-17 21:17:32 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cached_url",
-    version="0.0.12",
+    version="0.0.14",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Library for get url content (cached when testing).",
