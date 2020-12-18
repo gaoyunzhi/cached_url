685831e72452e5388bd408405757fe2052bdd6ed
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-17 22:05:30 -0800

    commit

--- test.py
+++ test.py
@@ -3,6 +3,6 @@
 
 import cached_url
 
-url = ''
-# cached_url.get(url, 'b')
+url = 'https://www.bbc.com/zhongwen/simp'
+# url = 'https://img9.doubanio.com/view/status/l/public/64f0c054cbc6f3d.jpg'
 print(cached_url.getFilePath(url))
