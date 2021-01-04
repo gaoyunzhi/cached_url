9ce956b2ea1954f9fe476e350450789110a2ee69
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-04 08:36:24 -0800

    commit

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ import cached_url
 
 if __name__ == '__main__':
 	url = 'https://www.coolloud.org.tw/'
-	url = 'https://www.evernote.com/l/AO_PIYii3ddBnZUE832bbYtnMkNqfJnZxOU?json=1'
+	# url = 'https://www.evernote.com/l/AO_PIYii3ddBnZUE832bbYtnMkNqfJnZxOU?json=1'
 	# url = 'https://img9.doubanio.com/view/status/l/public/64f0c054cbc6f3d.jpg'
 	cached_url.get(url)
