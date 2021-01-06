37b9eff47e3c15e7dddf66d6c8d85831d5aae957
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-01-07 03:31:06 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cached_url",
-    version="0.0.24",
+    version="0.0.26",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Library for get url content (cached when testing).",
