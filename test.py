e9964e780ba9befc057c2d1553795fe0eecd506d
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-06 19:28:07 -0800

    commit

--- test.py
+++ test.py
@@ -8,4 +8,5 @@ if __name__ == '__main__':
 	url = 'https://www.evernote.com/l/AO_PIYii3ddBnZUE832bbYtnMkNqfJnZxOU?json=1'
 	# url = 'https://img9.doubanio.com/view/status/l/public/64f0c054cbc6f3d.jpg'
 	# url = 'http://www.infzm.com/wap/#/content/198828'
+	url = 'https://wemp.app/accounts/e2eb0c1c-eca7-4602-9a92-3621df4d2ad6'
 	cached_url.get(url)
